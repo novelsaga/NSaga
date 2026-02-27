877c1dd580cd2a711e9c065a081d319e09203e98
zerozawa <zekaixie37@gmail.com>
2026-02-27 16:17:03 +0800

    refactor(cli): re-enable metadata module exports

--- projects/cli/src/metadata/mod.rs
+++ projects/cli/src/metadata/mod.rs
@@ -4,7 +4,11 @@ pub mod index;
 pub mod watcher;
 pub mod worker;
 
-// pub use cache::CacheManager;  // TODO: integrate into CLI commands
-// pub use index::IndexManager;   // TODO: integrate into CLI commands
-// pub use watcher::{FileChangeEvent, FileWatcher};  // TODO: integrate into CLI commands
-// pub use worker::{WriteBackWorker, WriteTask};  // TODO: integrate into CLI commands
+#[allow(unused_imports)]
+pub use cache::CacheManager;  // TODO: integrate into CLI commands
+#[allow(unused_imports)]
+pub use index::IndexManager;   // TODO: integrate into CLI commands
+#[allow(unused_imports)]
+pub use watcher::{FileChangeEvent, FileWatcher};  // TODO: integrate into CLI commands
+#[allow(unused_imports)]
+pub use worker::{WriteBackWorker, WriteTask};  // TODO: integrate into CLI commands
